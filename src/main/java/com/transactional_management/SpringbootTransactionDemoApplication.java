844e2ab554a17f1f53bfad394feeb289a0bde2b5
gr-rishad <gr.rishad@gmail.com>
2022-06-27 14:50:09 +0600

    project initialize

---
+++ src/main/java/com/transactional_management/SpringbootTransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.transactional_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionDemoApplication.class, args);
+	}
+
+}
